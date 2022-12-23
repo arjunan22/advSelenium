d93636d2e4b45321f62ce478f3f117ad7dc0a0da
arjunan22 <arjunan22@gmail.com>
2022-12-23 11:28:17 +0530

    login script- Vilcart-rmg

--- src/test/java/com/vilcart/app/loginCase.java
+++ src/test/java/com/vilcart/app/loginCase.java
@@ -1,10 +1,34 @@
 package com.vilcart.app;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class loginCase {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException, InterruptedException {
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+				
+		FileInputStream fis = new FileInputStream("./src/test/resources/data.properties");
+		Properties p =new Properties();
+		p.load(fis);
+		
+		driver.get(p.getProperty("url"));
+		driver.findElement(By.xpath("(//input[@class='text-input text-input-md'])[1]")) .sendKeys(p.getProperty("email"));
+		driver.findElement(By.xpath("(//input[@class='text-input text-input-md'])[2]")).sendKeys(p.getProperty("password"));
+		driver.findElement(By.xpath("(//span[@class='button-inner'])[5]")).click();
+			
+						
+		Thread.sleep(10000);
+		driver.close();
 	}
 
 }
