b48625de843f02168baeeeb43f4bc364d4027c28
arjunan22 <arjunan22@gmail.com>
2023-02-16 11:36:37 +0530

    edge driver

--- src/test/java/com/vilcart/app/loginCase.java
+++ src/test/java/com/vilcart/app/loginCase.java
@@ -14,6 +14,7 @@ public class loginCase {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		//WebDriver driver= new ChromeDriver();
+		System.setProperty("webdriver.edge.driver", "C:\\Users\\Admin\\Downloads\\msedgedriver.exe" );
 		WebDriver driver= new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
