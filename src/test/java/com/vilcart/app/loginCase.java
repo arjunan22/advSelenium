ef27a2ab07942f3cae791ae4e1bb782654e9ac07
arjunan22 <arjunan22@gmail.com>
2023-04-20 11:26:26 +0530

    testng

--- src/test/java/com/vilcart/app/loginCase.java
+++ src/test/java/com/vilcart/app/loginCase.java
@@ -8,11 +8,12 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 
 
 public class loginCase {
-
+@Test
 	public static void main(String[] args) throws IOException, InterruptedException {
 		//WebDriver driver= new ChromeDriver();
 		
